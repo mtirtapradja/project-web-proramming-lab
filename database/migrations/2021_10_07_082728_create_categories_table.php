8f9063a214145a5efa73ed9e2e3d3d8ca33f4c0b
mtirtapradja <mtirtapradja@gmail.com>
2021-10-07 15:34:42 +0700

    Add model, migration, and seeder for category

--- database/migrations/2021_10_07_082728_create_categories_table.php
+++ database/migrations/2021_10_07_082728_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGendersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateGendersTable extends Migration
      */
     public function up()
     {
-        Schema::create('genders', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateGendersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('genders');
+        Schema::dropIfExists('categories');
     }
 }

c3c1d962a812f0e765d4bb03038a3ae4a99052ec
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2021-10-07 14:28:03 +0700

    ss_1

---
+++ resources/views/addCategory.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>Add Category Page</h1>
+@endsection
---
+++ resources/views/search.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1>Search Page</h1>
+@endsection

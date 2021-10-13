ed1399cd0622a3e12d49f7503ba880faf4d3a5ed
mtirtapradja <mtirtapradja@gmail.com>
2021-10-13 16:03:13 +0700

    add all buttons for all user to navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,9 @@
     <header>
         <div class="row g-0 p-3 align-items-center color-primary">
             <div class="col-6 col-md-1 text-center">
-                <h1>DY.ID</h1>
+                <a href="/" class="text-decoration-none text-black">
+                    <h1>DY.ID</h1>
+                </a>
             </div>
             <div class="col-sm-6 col-md-11">
                 {{-- Add GET method action to... --}}
@@ -19,20 +21,63 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link text-light active" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Features</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Pricing</a>
+                            <a class="nav-link text-light active" href="/">Home</a>
                         </li>
                     </ul>
-                    <ul class="navbar-nav ms-auto">
+                    //TODO ini harus ada conditional rendering untuk user
+                    <ul class="navbar-nav">
                         <li class="nav-item">
+                            <a class="nav-link text-light active" href="/my-cart">My Cart</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-light active" href="/history">History Transaction</a>
+                        </li>
+                    </ul>
+                    //TODO ini harus ada conditional rendering untuk admin
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link text-light dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown">
+                                Manage Product
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="/add-product">Add Product</a></li>
+                                <li><a class="dropdown-item" href="/products">View Product</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link text-light dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown">
+                                Manage Category
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="/add-category">Add Category</a></li>
+                                <li><a class="dropdown-item" href="/categories">View Category</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    //TODO harus ada conditional rendering untuk tipe user apa
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item me-2">
                             <a href="/login" class="btn btn-danger">
                                 <i class="bi bi-box-arrow-in-right"></i>
                                 Login
+                            </a>
+                        </li>
+                        <li class="nav-item me-2">
+                            <a href="/register" class="btn btn-danger">
+                                <i class="bi bi-person-plus-fill"></i>
+                                Register
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/register" class="btn btn-danger">
+                                <i class="bi bi-box-arrow-left"></i>
+                                Logout
                             </a>
                         </li>
                     </ul>

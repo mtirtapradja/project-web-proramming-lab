3ca179d072baeef51f8dde15212021b5b6728a59
STEVEN SANTOSO SUNTIARKO <steven.suntiarko@binus.ac.id>
2021-10-14 13:13:30 +0700

    login register update

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
     //
     public function index(){
         return view('pages.Register');
+    }
+
+    public function store(Request $request){
+        //TODO konekin ke blade
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email:dns|unique:users',
+            'role_id' => 'required|min:1|max:2',
+            'gender_id' => 'required|min:1|max:2',
+            'address' => 'required|max:255',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        User::create($validatedData);
     }
 }

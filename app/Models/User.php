66b610316a1a5bef27ed0d468ad6e8640e4e26c4
mtirtapradja <mtirtapradja@gmail.com>
2021-10-19 00:44:23 +0700

    add authenticable to user model for using Auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 
-
-class User extends Model
+class User extends Model implements AuthenticatableContract
 {
-    use HasFactory;
+    use HasFactory, Authenticatable;
 
     function transactions()
     {

15321ab16d503786cd84eaab1addcc89e551250e
mtirtapradja <mtirtapradja@gmail.com>
2021-10-19 11:35:21 +0700

    fix detail product page style

--- resources/views/pages/product/detail-product.blade.php
+++ resources/views/pages/product/detail-product.blade.php
@@ -1,73 +1,60 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="container-fluid d-flex flex-row">
-        <img src="https://source.unsplash.com/random{{-- /750x750 --}}" alt="image" class="mg-fluid rounded-start"
+    {{-- <div class="container-fluid d-flex flex-row justify-content-center">
+        <img src="https://source.unsplash.com/random/750x750" alt="image" class="img-fluid rounded-start"
             style="height: 46.875em; width: 46.875em">
         <div class="bg-light">
             <div class="container d-flex align-items-center">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">
-                        <h1>SAMSUNG</h1>
+                        <p class="fs-4 fw-bold">{{ $product->name }}</p>
                     </li>
                     <li class="list-group-item">
                         Category
-                        <h1>HP</h1>
+                        <p class="fs-4 fw-bold">{{ $product->category->name }}</p>
                     </li>
                     <li class="list-group-item">
                         Price
-                        <h1>IDR ...</h1>
+                        <p class="fs-4 fw-bold">IDR {{ $product->price }}</p>
                     </li>
                     <li class="list-group-item">
                         Description
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident expedita harum alias vero
-                            veritatis cupiditate tempora iusto libero atque cum debitis, voluptas ipsam numquam cumque
-                            inventore mollitia accusamus accusantium sapiente?</p>
+                        <p>{{ $product->description }}</p>
                     </li>
                 </ul>
             </div>
         </div>
+    </div> --}}
+    <div class="container-fluid d-flex flex-row justify-content-center">
+        <div class="card mb-3" style="width: 100%;">
+            <div class="row g-0">
+                <div class="col-md-8">
+                    <img src="https://source.unsplash.com/random/750x750" alt="image" class="img-fluid rounded-start"
+                        style="height: 60em; width: 60em">
+                </div>
+                <div class="col-md-4">
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <p class="fs-4 fw-bold">{{ $product->name }}</p>
+                            </li>
+                            <li class="list-group-item">
+                                <p class="fs-4 fw-bold">Category:</p>
+                                <p class="fs-5 fw-light">{{ $product->category->name }}</p>
+                            </li>
+                            <li class="list-group-item">
+                                <p class="fs-4 fw-bold">Price:</p>
+                                <p class="fs-5 fw-light">IDR {{ $product->price }}</p>
+                            </li>
+                            <li class="list-group-item">
+                                <p class="fs-4 fw-bold">Description:</p>
+                                <p class="fs-5 fw-light">IDR {{ $product->description }}</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
-
-{{-- <form class="p-4" action="/login" method="post">
-        @csrf
-        <p class="fs-3 mb-3 text-primary">Join With Us</p>
-        <div class="mb-3">
-            <input type="text" name="name" class="form-control" placeholder="Full Name">
-        </div>
-        <div class="mb-3">
-            <input type="email" name="email" class="form-control" placeholder="Email">
-        </div>
-        <div class="mb-2">
-            <div class="text-start mb-1">
-                Gender
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" name="gender_id" type="radio" value="1">
-                <label class="form-check-label" for="inlineCheckbox1">Male</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" name="gender_id" type="radio" value="2">
-                <label class="form-check-label" for="inlineCheckbox2">Female</label>
-            </div>
-        </div>
-        <div class="mb-3">
-            <textarea class="form-control" name="address" rows="3" placeholder="Address" required></textarea>
-        </div>
-        <div class="mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password">
-        </div>
-        <div class="mb-3">
-            <input type="password" name="confirmPassword" class="form-control" placeholder="Confirm Password">
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="">
-            <label class="form-check-label" for="flexCheckChecked">
-                I agree with terms & condition
-            </label>
-        </div>
-        <div class="mt-3 text-end">
-            <button type="submit" class="btn btn-warning">Register Now</button>
-        </div>
-    </form> --}}

ee788bbeb5974adb05cad970707e7ed56b4e29de
mtirtapradja <mtirtapradja@gmail.com>
2021-12-01 14:48:16 +0700

    Add required field rules

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use GuzzleHttp\Promise\Create;
 use Illuminate\Contracts\Support\ValidatedData;
+use Illuminate\Contracts\Validation\Validator;
 
 class RegisterController extends Controller
 {
-    //
     public function index()
     {
         return view('pages.register', [
@@ -19,18 +19,22 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
+        // dd($request);
         $validatedData = $request->validate([
             'name' => ['required', 'max:255'],
             'email' => ['required', 'email:dns'],
-            'gender_id' => ['required'],
+            'gender_id' => ['required', 'in:1,2'],
             'address' => ['required', 'max:255'],
-            'password' => ['required', 'min:5', 'max:255']
+            'password' => ['required', 'min:5', 'max:255', 'confirmed']
         ]);
 
         $validatedData['password'] = bcrypt($validatedData['password']);
 
-        User::create($validatedData);
-
-        return redirect()->intended('/login')->with('success', 'You are registered, please login!');
+        if ($request->agree) {
+            User::create($validatedData);
+            return redirect()->intended('/login')->with('success', 'You are registered, please login!');
+        } else {
+            return back()->with('loginError', 'Login failed! You need to agree check the terms & condition');
+        }
     }
 }
--- resources/views/pages/Register.blade.php
+++ resources/views/pages/Register.blade.php
@@ -2,77 +2,99 @@
 
 @section('container')
     <div class="container">
-        <form class="bg-light p-4" action="/register" method="post">
-            @csrf
-            <p class="fs-3 mb-3 text-primary">Join With Us</p>
-            <div class="mb-3">
-                <input type="text" name="name" class="form-control  @error('name') is-invalid @enderror"
-                    placeholder="Full Name" required>
-                @error('name')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
+        <div>
+            <div class="row">
+                {{-- Buat nampilin flash message dari controller --}}
+                @if (session()->has('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                @endif
+
+                {{-- Buat nampilin flash message dari controller --}}
+                @if (session()->has('loginError'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('loginError') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                @endif
             </div>
-            <div class="mb-3">
-                <input type="email" name="email" class="form-control  @error('email') is-invalid @enderror"
-                    placeholder="Email" required>
-                @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-2">
-                <div class="text-start mb-1">
-                    Gender
+            <form class="bg-light p-4" action="/register" method="post">
+                @csrf
+                <p class="fs-3 mb-3 text-primary">Join With Us</p>
+                <div class="mb-3">
+                    <input type="text" name="name" class="form-control  @error('name') is-invalid @enderror"
+                        placeholder="Full Name" required>
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" name="gender_id" type="radio" value="1">
-                    <label class="form-check-label" for="inlineCheckbox1">Male</label>
+                <div class="mb-3">
+                    <input type="email" name="email" class="form-control  @error('email') is-invalid @enderror"
+                        placeholder="Email" required>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" name="gender_id" type="radio" value="2">
-                    <label class="form-check-label" for="inlineCheckbox2">Female</label>
+                <div class="mb-2">
+                    <div class="text-start mb-1">
+                        Gender
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" name="gender_id" type="radio" value="1" required>
+                        <label class="form-check-label" for="gender_id">Male</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" name="gender_id" type="radio" value="2" required>
+                        <label class="form-check-label" for="gender_id">Female</label>
+                    </div>
+                    @error('gender_id')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-            </div>
-            <div class="mb-3">
-                <textarea class="form-control  @error('address') is-invalid @enderror" name="address" rows="3"
-                    placeholder="Address" required></textarea>
-                @error('address')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <input type="password" name="password" class="form-control  @error('password') is-invalid @enderror"
-                    placeholder="Password" required>
-                @error('password')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="mb-3">
-                <input type="password" name="confirmPassword"
-                    class="form-control  @error('confirmPassword') is-invalid @enderror" placeholder="Confirm Password"
-                    required>
-                @error('confirmPassword')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="">
-                <label class="form-check-label" for="flexCheckChecked">
-                    I agree with terms & condition
-                </label>
-            </div>
-            <div class="mt-3 text-end">
-                <button type="submit" class="btn btn-warning">Register Now</button>
-            </div>
-        </form>
+                <div class="mb-3">
+                    <textarea class="form-control  @error('address') is-invalid @enderror" name="address" rows="3"
+                        placeholder="Address" required></textarea>
+                    @error('address')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <input type="password" name="password" class="form-control  @error('password') is-invalid @enderror"
+                        placeholder="Password" required>
+                    @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <input type="password" name="password_confirmation"
+                        class="form-control  @error('password_confirmation') is-invalid @enderror"
+                        placeholder="Confirm Password" required>
+                    @error('password_confirmation')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" name="agree" type="checkbox" value="checked">
+                    <label class="form-check-label" for="flexCheckChecked">
+                        I agree with terms & condition
+                    </label>
+                </div>
+                <div class="mt-3 text-end">
+                    <button type="submit" class="btn btn-warning">Register Now</button>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
